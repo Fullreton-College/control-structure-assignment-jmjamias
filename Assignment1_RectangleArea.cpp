317c9d99d346e4003b750300102852f9f3ff787d
Jamie Jamias <jamiemanabatjamias@gmail.com>
2026-02-20 09:58:10 -0800

    Assignment 1 Completed

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -3,7 +3,32 @@
 using namespace std;
 
 int main()
-{
+{ 
+    double length1, width1, length2, width2;
+    
+    cout << "Enter the length of the first rectangle: ";
+    cin >> length1;
+
+    cout << "Now, enter the width of the first rectangle: ";
+    cin >> width1;
+
+    cout << "Enter the length of the second rectangle: ";
+    cin >> length2; 
+
+    cout << "Now, enter the width of the second rectangle: ";
+    cin >> width2;
+
+    double area1 = length1 * width1;
+    double area2 = length2 * width2;
+
+    if(area1 > area2) {
+        cout << "The 1st rectangle has the greater area.";
+    } else if (area1 < area2) {
+        cout << "The 2nd rectangle has the greater area.";
+    } else {
+        cout << "The areas are the same.";
+    }
+
 	
     return 0;
 }
