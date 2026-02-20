6652786b5347d95a05d6e10358ad63467112b0bf
Jamie Jamias <jamiemanabatjamias@gmail.com>
2026-02-20 10:18:59 -0800

    Assignment 2 Completed

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -3,14 +3,28 @@
 using namespace std;
 int main()
 {
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
+  int unit;
+  double discount, total; 
 
-    //express pace in minutes and seconds
-  /* write your code here */
+  cout << "Enter the number of units purchased: ";
+  cin >> unit;
+ 
+  if (unit >= 10 && unit <= 19) {
+    discount = 0.20; 
+  } else if (unit >= 20 && unit <= 40) {
+    discount = 0.30;
+  } else if (unit >= 50 && unit <= 99) {
+    discount = 0.40;
+  } else if (unit >= 100) {
+    discount = 0.50;
+  } else {
+    discount = 0.0;
+  }
+  total = 199 * unit * (1 - discount);
 
-  //express your output in 1 decimal points
+  cout << fixed << setprecision(2);
 
-
+  cout << "Total cost: $" << total << endl;
+  
+return 0;
 }
